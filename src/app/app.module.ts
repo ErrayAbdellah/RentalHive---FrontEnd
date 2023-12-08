17db3c92cdbe3535f6790c186cfaeca6808f7143
Boutaina <boutaina.elatbaoui@gmail.com>
2023-12-08 11:18:18 +0100

    Modified the navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { EquipmentComponent } from './components/equipment/equipment.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ReservationComponent } from './reservation/reservation/reservation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomePageComponent,
-    EquipmentComponent
+    EquipmentComponent,
+    ReservationComponent,
   ],
   imports: [
     BrowserModule,
