f4de66a944a1a830c6f21d7e8385d9b41480d0e6
ErrayAbdellah <mahdiabdllah121@gmail.com>
2023-12-09 20:47:34 +0100

    update service equipment

--- src/app/Services/upadate-equipment/update-equipment.service.ts
+++ src/app/Services/upadate-equipment/update-equipment.service.ts
@@ -11,7 +11,7 @@ export class UpdateEquipmentService {
 
   onInite(){}
 
-  getEquipmentById():Equipment{
-    return this.http.get() ;
-  }
+  // getEquipmentById():Equipment{
+    // return this.http.get() ;
+  // }
 }
